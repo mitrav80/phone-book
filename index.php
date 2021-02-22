738efaf311246b3e920dec50287e5c281aed8606
Pouya Amani <pouya1364@gmail.com>
2021-02-22 16:35:40 +0800

    create index

--- index.php
+++ index.php
@@ -4,15 +4,42 @@ $connect = mysqli_connect('localhost','root', '');
 if ($connect->connect_error) {
     die("Connection failed: " . $connect->connect_error);
   }
-  echo "Connected successfully";
   
 $sql = "select * from phonebook.details";
 $result = mysqli_query($connect, $sql);
 if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-      echo "first name: " . $row["Firstname"]. "last Name: " . $row["Lastname"]. "Phone: " . $row["Phone"]. "email: " .$row["email"] "<br>";
-    }
+
+    echo "<table border='1'>
+
+<tr>
+
+<th>First Name</th>
+
+<th>Last Name</th>
+
+<th>Phone Number</th>
+
+<th>Email Adress</th>
+
+</tr>";
+
+ while($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+
+    echo "<td>" . $row["Firstname"] . "</td>";
+      
+    echo "<td>" . $row["Lastname"] . "</td>";
+      
+    echo "<td>" . $row['Phone'] . "</td>";
+      
+    echo "<td>" . $row['Email'] . "</td>";
+      
+     echo "</tr>";
+      
+        }
+      
+      echo "</table>";
+
   } else {
     echo "0 results";
   }
