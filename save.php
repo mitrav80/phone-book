17a0409da15d828d00c7aa2c8d0374d8b61b5550
Pouya Amani <pouya1364@gmail.com>
2021-03-22 17:28:43 +0800

    modify save page

--- save.php
+++ save.php
@@ -4,43 +4,43 @@ $LastName ='';
 $phone ='';
 $email ='';
 
-if ($_POST['FirstName']) {
+if (isset($_POST['FirstName'])) {
     $firstName = $_POST['FirstName'];
+    echo $firstName;    
 }
 
-if ($_POST['LastName']) {
+if (isset($_POST['LastName'])) {
     $LastName = $_POST['LastName'];
 }
-if ($_POST['phone']) {
+if (isset($_POST['phone'])) {
     $phone = $_POST['phone'];
 }
-if ($_POST['Email']) {
+if (isset($_POST['Email'])) {
     $email = $_POST['Email'];
 }
 
-if  ($firstname && $lastname && $phone && $email ){
+if  ($firstName && $LastName && $phone && $email ){
+    
+    $connect = mysqli_connect('localhost','root', '');
 
+    if ($connect->connect_error) {
+        die("Connection failed: " . $connect->connect_error);
+    }
+    echo "Connected successfully";
+    
+    $sql = "INSERT INTO phonebook.details (firstname, lastname,phone, email)
+    VALUES ('$firstName', '$LastName','$phone','$email')";
 
-$connect = mysqli_connect('localhost','root', '');
+    if(mysqli_query($connect, $sql)){
+        echo "Records inserted successfully.";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($connect);
+    }
 
-if ($connect->connect_error) {
-    die("Connection failed: " . $connect->connect_error);
-  }
-  echo "Connected successfully";
-  
-  $sql = "INSERT INTO phonebook.details (firstname, lastname,phone, email)
-VALUES ('$firstName', '$LastName','$phone','$email')";
-
-if(mysqli_query($connect, $sql)){
-    echo "Records inserted successfully.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($connect);
-}
-
-$connect->close();
+    $connect->close();
 
 }
 header("Location: http://localhost/mitra/phone-book/");
-?>
+
 
   
